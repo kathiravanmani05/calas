4e7cb24d13166daf4462508812e0c29d7bf48083
kathiravanmani05 <162774265+kathiravanmani05@users.noreply.github.com>
2024-06-05 22:04:59 +0530

    updtaed

--- calas/calas/pipelines.py
+++ calas/calas/pipelines.py
@@ -1,13 +1,13 @@
 import logging
+import json
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from calas.models import Image, Product,Category
+from calas.models import Image, Product, Category
 
 class CalasPipeline:
     def __init__(self):
         # Create a connection to the database
         self.engine = create_engine('mariadb+mariadbconnector://calas:T2iEYtVA6QdaQIEe@localhost/calas')
-        #self.engine = create_engine('mysql://root:root@localhost/calas')
         self.Session = sessionmaker(bind=self.engine)
         logging.basicConfig(level=logging.INFO)
         self.logger = logging.getLogger(__name__)
@@ -17,8 +17,16 @@ class CalasPipeline:
         with self.Session() as session:
             try:
                 main_image_url = item['images'][0].split('/')[-1] if item['images'] else None
-                main_category= item['category_id'][0]  if item['category_id'] else None
+                main_category = item['category_id'][0] if item['category_id'] else None
                 pdf = item['pdf'].split('/')[-1] if item['pdf'] else None
+
+                # Convert lists to JSON strings
+                material = json.dumps(item['material']) if item['material'] else None
+                color = json.dumps(item['color']) if item['color'] else None
+                weight = json.dumps(item['weight']) if item['weight'] else None
+                dimensions = json.dumps(item['dimensions']) if item['dimensions'] else None
+                stock = json.dumps(item['stock']) if item['stock'] else None
+                pdf = json.dumps(item['pdf']) if item['pdf'] else None
 
                 # Query the database for the record you want to update
                 record = session.query(Product).filter_by(url=item['url']).first()
@@ -28,18 +36,16 @@ class CalasPipeline:
                     self.logger.info(f"Updating existing product with URL: {item['name']}")
                     record.price = item['price']
                     record.name = item['name']
-                    #record.main_category = main_category
                     record.description = item['description']
                     record.url = item['url']
-                    record.Material = item['material']
-                    record.color = item['color']
-                    record.Weight = item['weight']
-                    record.Size = item['dimensions']
-                    record.Stock = item['stock']
+                    record.Material = material
+                    record.color = color
+                    record.Weight = weight
+                    record.Size = dimensions
+                    record.Stock = stock
                     record.pdf = pdf
                     record.main_image_url = main_image_url
                     spider.logger.info("Record updated successfully.")
-                    #import pdb;pdb.set_trace()
 
                 else:
                     self.logger.info(f"Creating new product with URL: {item['name']}")
@@ -49,13 +55,12 @@ class CalasPipeline:
                         name=item['name'],
                         price=item['price'],
                         description=item['description'],
-                        #main_category=main_category,
-                        Material = ['material'],
-                        color = ['color'],
-                        Weight =['weight'],
-                        Size = ['dimensions'],
-                        Stock = ['stock'],
-                        pdf = ['pdf'],
+                        Material=material,
+                        color=color,
+                        Weight=weight,
+                        Size=dimensions,
+                        Stock=stock,
+                        pdf=pdf,
                         main_image_url=main_image_url
                     )
                     session.add(new_record)
@@ -84,14 +89,14 @@ class CalasPipeline:
                 for category in item['category_id']:
                     category_value = category
                     cat_record = session.query(Category).filter_by(category=category_value).first()
-                    if not img_record:
-                        self.logger.info(f"Creating new image record for URL: {item['name']} with image URL: {category_value}")
+                    if not cat_record:
+                        self.logger.info(f"Creating new category record for URL: {item['name']} with category: {category_value}")
                         cat_record = Category(category=category_value, name=item['name'])
                         session.add(cat_record)
                         session.commit()
                         spider.logger.info("Category record created successfully.")
                     else:
-                        self.logger.info(f"Updating existing image record for URL: {item['name']} with image URL: {category_value}")
+                        self.logger.info(f"Updating existing category record for URL: {item['name']} with category: {category_value}")
                         cat_record.category = category_value
                         session.add(cat_record)
                         session.commit()
